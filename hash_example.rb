08407d67bceee6ca16a7f7fc2da25bfdbb6f249c
sanjay <sanjay827985>
2022-11-16 15:38:10 +0530

    new practice

---
+++ hash_example.rb
@@ -0,0 +1,11 @@
+person_info =[
+    'sanjay','kumar','skumar0443@gmail.com','INDIA'
+]
+
+person_info = {
+    first_name: 'ravi',
+    last_name: 'kumar',
+    email: 'ravi@123.com',
+    contact: '1234566',
+    address: 'indore'
+}
